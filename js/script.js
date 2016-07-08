dfc798fd2e76573b9fcc460ad1ba2776e62e3150
Minh Hoang <minh.le@jmango360.com>
2016-07-08 10:09:52 +0700

    Update question frame 3

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
   var wow = new WOW(
     {
-      boxClass:     'wow',      // animated element css class (default is wow)
+      boxClass: 'wow',      // animated element css class (default is wow)
       animateClass: 'animated', // animation css class (default is animated)
-      offset:       0,          // distance to the element when triggering the animation (default is 0)
-      mobile:       true,       // trigger animations on mobile devices (default is true)
-      live:         true,       // act on asynchronously loaded content (default is true)
-      callback:     function(box) {
+      offset: 0,          // distance to the element when triggering the animation (default is 0)
+      mobile: true,       // trigger animations on mobile devices (default is true)
+      live: true,       // act on asynchronously loaded content (default is true)
+      callback: function (box) {
         // the callback is fired every time an animation is started
         // the argument that is passed in is the DOM node being animated
       },
@@ -19,22 +19,69 @@ $(document).ready(function () {
   });
 
   var dialogLogin = $('#loginWithFacebook');
-  $('.open').click(function(e){
+  $('.open').click(function (e) {
     dialogLogin.removeClass('dialog-close');
     dialogLogin.addClass('dialog-open');
   });
-  $('.btn-login-with-facebook, .close-dialog-login, .dialog-overlay').click(function(e){
+  $('.btn-login-with-facebook, .close-dialog-login, .dialog-overlay').click(function (e) {
     dialogLogin.removeClass('dialog-open');
     dialogLogin.addClass('dialog-close');
   });
 
   var dialogGallery = $('#dialogGallery');
-  $('.btn-login-with-facebook, .view-gallery').click(function(e){
+  $('.btn-login-with-facebook, .view-gallery').click(function (e) {
     dialogGallery.removeClass('dialog-close');
     dialogGallery.addClass('dialog-open');
   });
-  $('.close-dialog-gallery').click(function(e){
+  $('.close-dialog-gallery').click(function (e) {
     dialogGallery.removeClass('dialog-open');
     dialogGallery.addClass('dialog-close');
+  });
+
+  var showQuestion = $('.block-image');
+  var goBack = $('.btn-back');
+  var answerByUser = false;
+  var result = '';
+
+  showQuestion.click(function (e) {
+    e.preventDefault();
+    if(!answerByUser){
+      $(this).find('.isOpen').parents('.frame3').find('#question-list').hide();
+      $(this).parents('.frame3').find('#question').show();
+    }else{
+      if(result === 'reward'){
+        $(this).find('.isOpen').parents('.frame3').find('#question-list').hide();
+        $(this).parents('.frame3').find('#reward').show();
+      }else{
+        $(this).find('.isOpen').parents('.frame3').find('#question-list').hide();
+        $(this).parents('.frame3').find('#lost').show();
+      }
+    }
+  });
+
+  showQuestion.click(function (e) {
+    e.preventDefault();
+    $(this).find('.isOver').parents('body').find('#dialogGallery').removeClass('dialog-close').addClass('dialog-open');
+  });
+
+
+  goBack.click(function (e) {
+    e.preventDefault();
+    $(this).parents('.frame3').find('#sub-block').hide();
+    $(this).parents('.frame3').find('#question-list').show();
+  });
+
+  $("form#question-data").submit(function (event) {
+    event.preventDefault();
+    answerByUser = true;
+    if ($("input[name=optionsRadios]:checked").val() === "A") {
+      result = 'reward';
+      $(this).parents('.frame3').find('#question').hide();
+      $(this).parents('.frame3').find('#reward').show();
+    } else {
+      result = 'lost';
+      $(this).parents('.frame3').find('#question').hide();
+      $(this).parents('.frame3').find('#lost').show();
+    }
   });
 });
